0bdd69496ca0507df93a4f57ffd7f772220ffdc5
Steven Engelbretson <sje779@my.utexas.edu>
2026-01-22 20:26:43 +0000

    continued readability work

--- homework02/exercise2.py
+++ homework02/exercise2.py
@@ -2,13 +2,13 @@ from Bio.Seq import seq
 
 def main():
     dna_seq = Seq("GAACCGGGAGGTGGGAATCCGTCACATATGAGAAGGTATTTGCCCGATAA")
-    mrna = dna_seq.transcribe()
+    mrna_seq = dna_seq.transcribe()
     stop = ["UAA", "UAG", "UGA"]
     
     stop_count = 0
     found_dict = {}
     for position in range(len(dna_seq) - 2):
-        codon = mrna[position: position + 2]
+        codon = mrna_seq[position: position + 2]
         if codon in stop:
             stop_count += 1
             found_dict[position + 1] = codon
