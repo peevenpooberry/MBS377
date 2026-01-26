68b5be671973e4aaeb3401dae1dd777cead9df36
peevenpooberry <sje779@my.utexas.edu>
2026-01-26 00:07:55 -0600

    Fix typo in even number print statement

--- homework02/exercise1.py
+++ homework02/exercise1.py
@@ -3,7 +3,7 @@ def main():
     num_list = [1,2,3,4,5,6,7,8,9,10]
         for i in num_list:
             if i % 2 == 0:
-                print(f"{i} i even")
+                print(f"{i} is even")
             else:
                 print(f"{i} is odd")
 
