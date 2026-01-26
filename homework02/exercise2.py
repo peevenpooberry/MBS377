56b2976f5c74f5c1fb5b6474aaebd1dba4bf17d6
peevenpooberry <sje779@my.utexas.edu>
2026-01-26 00:12:17 -0600

    Fix codon extraction for mRNA sequence

--- homework02/exercise2.py
+++ homework02/exercise2.py
@@ -7,8 +7,8 @@ def main():
     
     stop_count = 0
     found_dict = {}
-    for index in range(len(dna_seq) - 2):
-        codon = mrna_seq[index: index + 2]
+    for index in range(len(mrna_seq) - 2):
+        codon = mrna_seq[index: index + 3]
         if codon in stop:
             stop_count += 1
             position = index + 1
