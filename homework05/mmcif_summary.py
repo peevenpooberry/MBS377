4983a751576d307f7a7e61b6ec2c5ac572c31481
Steven Engelbretson <sje779@my.utexas.edu>
2026-02-12 21:42:43 +0000

    program successfuly works

--- homework05/mmcif_summary.py
+++ homework05/mmcif_summary.py
@@ -8,8 +8,8 @@ import json
 # -------------------------
 # Constants (configuration)
 # -------------------------
-INPUT_FILE = ""
-OUTPUT_FILE = ""
+INPUT_FILE = "4HHB.cif"
+OUTPUT_FILE = "4HHB_summary.json"
 
 # -------------------------
 # Logging (configuration)
@@ -32,7 +32,7 @@ logging.basicConfig(level=args.loglevel, format=format_str)
 # -------------------------
 # Functions
 # -------------------------
-def open_input_file(INPUT_FILE: str)-> Structure:
+def open_input_file(INPUT_FILE: str) -> 'Bio.PDB.Structure.Structure':
     """
     Opens the given input mmCIF file and converts it to a Structure class from Bio.PDB.MMCIFParser 
     via parser.
@@ -47,20 +47,15 @@ def open_input_file(INPUT_FILE: str)-> Structure:
     parser = MMCIFParser()
     try:
         with open(INPUT_FILE, "r") as f:
-            try:
-                structure = parser.get_structure("", f)
-            except Exception as e:
-                logging.error(f"Incorrect input file format provided!\n{e}")
-                sys.exit(1)
-            
-            logging.info("Successfully opened input file")
+            structure = parser.get_structure("", f)
+            logging.info(f"Successfully opened {INPUT_FILE}")
         return structure
     except FileNotFoundError:
-        logging.error("Input file not found, ending process")
+        logging.error(f"{INPUT_FILE} not found, ending process")
         sys.exit(1)
 
 
-def parse_file(structure: Structure)-> list[dict]:
+def parse_file(structure:'Bio.PDB.Structure.Structure')-> list[dict]:
     """
     Iterates through the MMCIF.Parser and returns a summary about the chains within.
 
@@ -68,13 +63,13 @@ def parse_file(structure: Structure)-> list[dict]:
         structure: Structure, the file formatted by the Bio.PDB.MMCIFParser
 
     Returns:
-        chains: list[dict], a list of entries of chain summaries of dicts containing:
+        summary: list[dict], a list of entries of chain summaries of dicts containing:
             "chain_id": str
             "total_residues": int
             "standard_residues": int
             "hetero_residues": int
     """
-    chains = []
+    summary = []
 
     logging.info("Beginning to parse file")
     for model in structure:
@@ -91,8 +86,9 @@ def parse_file(structure: Structure)-> list[dict]:
             
             logging.debug(f"Parsing chain {chain_id}")
             for residue in chain:
+                id = residue.get_id()
                 hetfield = id[0]
-                if hetfield == "":
+                if hetfield == " ":
                     res_count += 1
                 else:
                     hetero_count += 1
@@ -101,19 +97,19 @@ def parse_file(structure: Structure)-> list[dict]:
             new_entry["total_residues"] = total_residues
             new_entry["standard_residues"] = res_count
             new_entry["hetero_residues"] = hetero_count
-            chains.append(new_entry)
+            summary.append(new_entry)
     
-    logging.info(f"Finished parsing {Input_File} have {len(chains)} chains")
-    return chains  
+    logging.info(f"Finished parsing {INPUT_FILE} have {len(summary)} chains")
+    return summary 
 
 
-def generate_output_file(OUTPUT_FILE: str, data: list[dict]):
+def generate_output_file(OUTPUT_FILE: str, summary: list[dict]):
     """
     Description.
 
     Args:
         OUTPUT_FILE: str, The name of the output json file.
-        data: list[dict], a list of entries of chain summaries of dicts containing:
+        summary: list[dict], a list of entries of chain summaries of dicts containing:
             "chain_id": str
             "total_residues": int
             "standard_residues": int
@@ -129,20 +125,20 @@ def generate_output_file(OUTPUT_FILE: str, data: list[dict]):
         logging.debug("Missing output directory")
         full_path = OUTPUT_FILE
     finally:
-        wrapper = {"chains": data}
-            with open(full_path, 'w') as outfile:
-                logging.debug(f"Loading to {OUTPUT_FILE}")
-                try:
-                    json.dump(summary.model_dump(), outfile, indent=2)
-                except Exception as e:
-                    logging.error(f"Writing to {OUTPUT_FILE} failed:\n{e}")
+        wrapper = {"chains": summary}
+        with open(full_path, 'w') as outfile:
+            logging.debug(f"Loading to {OUTPUT_FILE}")
+            try:
+                json.dump(wrapper, outfile, indent=2)
+            except Exception as e:
+                logging.error(f"Writing to {OUTPUT_FILE} failed:\n{e}")
 
 
 def main():
     logging.info("Beginning mmCIF Summary workflow")
     structure = open_input_file(INPUT_FILE)
-    data = parse_file(structure)
-    generate_output_file(OUTPUT_FILE, data)
+    summary = parse_file(structure)
+    generate_output_file(OUTPUT_FILE, summary)
     logging.info("mmCIF Summary workflow is complete!")
 
 
